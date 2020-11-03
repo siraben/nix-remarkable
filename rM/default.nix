49421a2c023432524329f0816923aa274f242545
Ben Siraphob <bensiraphob@gmail.com>
2020-11-03 13:47:06 +0700

    Comment out cross overlay

--- rM/default.nix
+++ rM/default.nix
@@ -4,10 +4,12 @@
 (import nixpkgs {
   system = "x86_64-linux";
   config.allowUnsupportedSystem = true;
-  crossSystem = import ./system.nix { lib = import <nixpkgs/lib>; };
+  crossSystem = import ./system.nix {
+    lib = (import <nixpkgs/lib>);
+  };
   overlays = [
     # Basic tools---the toolchain packages
-    (import ./toolchain.nix)
+    # (import ./toolchain.nix)
     # Upref to host packages, for symmetry's sake
     (self: super: { inherit hostPkgs; })
   ];
@@ -15,11 +17,11 @@
     # Basic rM packages---chiefly the Linux kernel and headers. These
     # need to go here in order to prevent the build toolchain being
     # built with glibc built against the rM kernel headers.
-    (import ./cross-overlay.nix)
+    # (import ./cross-overlay.nix)
     # Try to link most things statically, to reduce the number/net
     # size of files that must be deployed
     (import ./static.nix)
-    (import <nixpkgs/pkgs/top-level/static.nix>)
+    # (import <nixpkgs/pkgs/top-level/static.nix>)
     # Overrides to make more things work (hopefully)
     # Most of this should probably be moved into crossOverlays
     (import ./overrides.nix)
